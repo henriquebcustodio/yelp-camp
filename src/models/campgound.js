06b6778064cf5d48044993777675a65741565300
henriquebcustodio <henriquebcustodio@outlook.com>
2021-10-19 20:19:16 -0300

    Removed validations from the mongoose schema

--- src/models/campgound.js
+++ src/models/campgound.js
@@ -2,27 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
+    title: String,
     image: String,
-    price: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    reviews: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Review'
-    }]
+    price: Number,
+    description: String,
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
